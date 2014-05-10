d748f2098fea0cf165cae379bb11999aacab7e04
Harihar Subramanyam <hsubrama@mit.edu>
2014-05-10 09:58:32 -0400

    removed custom fetch

--- app/js/app/collections/eventset.js
+++ app/js/app/collections/eventset.js
@@ -7,16 +7,7 @@ define([
 ], function($, _, Backbone, HotelEvent, moment){
 	var EventSet = Backbone.Collection.extend({
 		url: "http://mel-devel.media.mit.edu/sixdegrees/api/events/?format=json",
-		model: HotelEvent,
-		custom_fetch: function() {
-			$.ajax({
-				method: "GET",
-				url: "http://mel-devel.media.mit.edu/sixdegrees/api/events/?format=json",
-				success: function(data){
-					console.log(data)
-				}
-			})
-		}
+		model: HotelEvent
 	});
 	return new EventSet();
 });
