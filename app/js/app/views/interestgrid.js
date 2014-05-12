fd471d0101e720c467cb7bb94303140a01e55766
Harihar Subramanyam <hsubrama@mit.edu>
2014-05-12 14:27:49 -0400

    Started interest parsing

--- app/js/app/views/interestgrid.js
+++ app/js/app/views/interestgrid.js
@@ -9,37 +9,61 @@ define([
   var InterestGridView = Backbone.View.extend({
     tagName: 'div',
     template: _.template(interestGridTemplate),
+    interests: null,
     initialize: function(){
-
+        var self = this;
+        $.ajax({
+            method: "GET",
+            url: "http://mel-devel.media.mit.edu/sixdegrees/publicdisplay/screen_two/",
+            success: function(data){
+                self.interests = data[Math.round(Math.random()*data.length)];
+                self.render();
+            }
+        });
     },
     render: function(){
-      this.$el.html(this.template());
-			// var temp = "<div class='brick' style='width:{width}px; height: {height}px; background-color: {color}'><div class='cover'>Demo fit zone</div></div>";
+        if(this.interests === null || this.interests === undefined){
+            return;
+        }
 
-			// var w = 1, h = 1, html = '', color = '', limitItem = 28;
-			// for (var i = 0; i < limitItem; ++i) {
-			// 	h = 1 + 3 * Math.random() << 0;
-			// 	w = 1 + 3 * Math.random() << 0;
-			// 	html += temp.replace(/\{height\}/g, h*150).replace(/\{width\}/g, w*150).replace("{color}", "rgb(142, 68, 173)");
-			// }
-			// $("#freewall").html(html);
+        this.$el.html(this.template({
+            interests: this.interests,
+            title_for_interest: this.title_for_interest
+        }));
+        var wall = new freewall("#freewall");
+        wall.reset({
+            selector: '.brick',
+            animate: false,
+            cellW: 160,
+            cellH: 160,
+            delay: 30,
+            onResize: function() {
+                wall.refresh();
+            }
+        });
+        // caculator width and height for IE7;
+        wall.fitZone();
+    },
+    title_for_interest: function(interest){
+        if(interest.location_name !== undefined){
+            return interest.n + " come(s) from " + interest.location_name;
+        }
 
+        if(interest.company_name !== undefined){
+            return interest.n + " work(s) for " + interest.company_name;
+        }
 
-			$(function() {
-				var wall = new freewall("#freewall");
-				wall.reset({
-					selector: '.brick',
-					animate: false,
-					// cellW: 160,
-					// cellH: 160,
-					delay: 30,
-					onResize: function() {
-						wall.refresh();
-					}
-				});
-				// caculator width and height for IE7;
-				wall.fitZone();
-			});
+        if(interest.industry_name !== undefined){
+            return interest.n + " work(s) in " + interest.industry_name;
+        }
+
+        if(interest.interest_name !== undefined){
+            return interest.n + " like " + interest.interest_name;
+        }
+
+        if(interest.school_name !== undefined){
+            return interest.n + " studied at " + interest.school_name;
+        }
 
     }
   });
